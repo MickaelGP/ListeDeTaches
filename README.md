59e9e44a1b083308f87c158738f5f8b697103018
unknown <mgperret@icloud.com>
2025-03-07 09:01:01 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Gestionnaire de Tâches Simple
+Ce projet est une application web simple pour gérer une liste de tâches (to-do list). Il permet aux utilisateurs d'ajouter et de supprimer des tâches, 
+qui sont stockées dans le localStorage du navigateur.
+
+# Fonctionnalités
+* Ajouter une nouvelle tâche.
+* Supprimer une tâche existante.
+* Afficher la liste des tâches stockées dans le localStorage.
+
+# Comment ça marche
+
+### Ajouter une tâche
+* L'utilisateur entre une tâche dans le champ d'entrée (input).
+* En cliquant sur le bouton "Ajouter", la tâche est ajoutée au localStorage si elle n'existe pas déjà.
+* La liste des tâches est mise à jour pour afficher la nouvelle tâche.
+
+### Supprimer une tâche
+* Chaque tâche affichée a un bouton "Supprimer".
+* En cliquant sur ce bouton, la tâche correspondante est supprimée du localStorage et la liste des tâches est mise à jour.
+
+# Utilisation
+1. Ouvrez le fichier HTML dans un navigateur web.
+2. Entrez une tâche dans le champ d'entrée et cliquez sur "Ajouter".
+3. La tâche apparaîtra dans la liste.
+4. Pour supprimer une tâche, cliquez sur le bouton "Supprimer" à côté de la tâche.
+
+## Auteur
+
+- MickaelGP - https://github.com/MickaelGP
+
+## License
+
+Ce projet est sous licence MIT. Pour plus d'informations, consultez le fichier [LICENSE](LICENSE).
---
+++ index.js
@@ -0,0 +1,55 @@
+const input = document.getElementById("input-todo");
+const btn = document.getElementById("btn-ajout-todo");
+const listeTache = document.getElementById("liste-tache");
+
+afficheData()
+
+btn.addEventListener("click", (event) => {
+    //let numero = "liste-"+localStorage.length;
+    if(localStorage.getItem(input.value.trim()) !== input.value) {
+        localStorage.setItem(input.value.trim(), input.value.trim())
+        console.log(localStorage);
+        input.value = "";
+        listeTache.innerHTML = ""
+        afficheData()
+    }else{
+        alert("Tâche déjà présente")
+    }
+})
+
+function CreateLi(value){
+    const li = document.createElement("li");
+    li.className = 'li-tache';
+    li.dataset.listeId = `${value}`;
+    li.textContent = value;
+    listeTache.appendChild(li);
+    li.appendChild(createBtn());
+}
+function createBtn(){
+    const newBtn = document.createElement("button");
+    newBtn.className = "deleteBtn";
+    newBtn.textContent = "Supprimer";
+    newBtn.addEventListener("click", (event) => {
+        console.log(newBtn.parentElement.dataset.listeId);
+        let keyStorage = newBtn.parentElement.dataset.listeId
+        localStorage.removeItem(keyStorage);
+        listeTache.innerHTML = ""
+        afficheData()
+    })
+    return newBtn;
+}
+
+
+function afficheData(){
+    let number = localStorage.length;
+    let liste = []
+    for (let i = 0; i < number; i++) {
+       liste.push(localStorage.key(i));
+    }
+    liste.forEach((item, key) => {
+        console.log(item);
+     CreateLi(item);
+   })
+
+
+}
